58577ee0623f64305460c213de29e3800a0c4a8e
Apri Supandi Pasaribu <162102625+BgApii@users.noreply.github.com>
2024-09-09 13:35:17 +0700

    Update Profile.java

--- Profile.java
+++ Profile.java
@@ -5,10 +5,13 @@ public class Profile {
     String NIM(){
         return "12350113279";
     }
-
+    String hobi(){
+        return "Bermian game, belajar, menonton film, and membaca komik";
+    }
     public static void main(String[] args) {
         Profile profile = new Profile();
         System.out.println("Nama\t: " + profile.nama());
         System.out.println("NIM \t: " + profile.NIM());
+        System.out.println("Hobi \t: " + profile.hobi());
     }
 }
